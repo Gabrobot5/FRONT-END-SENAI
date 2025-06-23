de30754ffb2ca709eb0a6eca57011f07f2833813
Gabrobot5 <gabz343ind@gmail.com>
2025-06-23 11:34:35 -0300

    javascript

---
+++ 04 - javascript/aula 1 - introdução/scripts/teste.js
@@ -0,0 +1,29 @@
+console.log("Olá mundo!");
+
+nome = "Gab";
+
+console.log(nome);
+
+numero = 1;
+valorReais = 10.10;
+ligado = true;
+lista = [1]
+
+console.log(typeof(nome));
+console.log(typeof(numero));
+console.log(typeof(valorReais));
+console.log(typeof(ligado));
+console.log(typeof(lista));
+
+var teste = "Oi";
+var teste = "Oi";
+
+// alert("que legal");
+// confirm("DESEJA BAIXAR NOVAMENTE?");
+// prompt("Digite o seu nome");
+
+console.clear();
+
+alert("Script de soma");
+prompt()
+
