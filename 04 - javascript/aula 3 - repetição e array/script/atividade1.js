184f02d1263067d83e3b93e6f9d25a67a2f3ac6f
Gabrobot5 <gabz343ind@gmail.com>
2025-06-26 11:47:38 -0300

    aula 3

---
+++ 04 - javascript/aula 3 - repetição e array/script/atividade1.js
@@ -0,0 +1,41 @@
+
+const numero = parseInt(prompt("Digite o número para a tabuada:"));
+const limite = parseInt(prompt("Até qual número deseja multiplicar?"));
+
+
+if (isNaN(numero) || isNaN(limite)) {
+    alert("Por favor, digite números válidos!");
+} else {
+    
+    let tabela = `
+        <table id="tabuada">
+            <thead>
+                <tr>
+                    <th colspan="3">Tabuada do ${numero}</th>
+                </tr>
+                <tr>
+                    <th>Operação</th>
+                    <th>Resultado</th>
+                </tr>
+            </thead>
+            <tbody>
+    `;
+
+
+    for (let i = 1; i <= limite; i++) {
+        tabela += `
+            <tr>
+                <td>${numero} × ${i}</td>
+                <td>${numero * i}</td>
+            </tr>
+        `;
+    }
+
+    tabela += `
+            </tbody>
+        </table>
+    `;
+
+    
+    document.getElementById("resultado").innerHTML = tabela;
+}
--- 04 - javascript/aula 3 - repetição e array/script/material.js
+++ 04 - javascript/aula 3 - repetição e array/script/material.js
@@ -1,6 +1,36 @@
 // enquanto () faca ... !controlador
-var i = 0;
+// i = contador
+// WHILE - 
+var i = 3;
+console.log("-----------WHILE----------")
 while (i < 3) {
     console.log(`Eu repeti ${i} vezes`);
     i++;
 }
+
+// DO WHILE -FAZ UMA VEZ E REPETE APENAS QUANDO A CONDIÇÃO FOR FAVORÁVEL
+i = 3;
+console.log("---------DO WHILE---------")
+do{
+    console.log(`Eu repeti ${i+1} vezes`)
+    i++
+}while (i < 3);
+
+console.log("------------FOR-----------")
+for(i = 0;i < 10;i++){
+    console.log(`Repeti ${i+1} vezes`)
+}
+
+// FOR EACH
+var alunos = ['Victor','Rafael','Bruno','Alessandra'];
+var contador = 0
+alunos.forEach(cadaAluno => {
+    document.querySelector("table").innerHTML += 
+    <tr>
+        <th> ${contador+1} </th> 
+        <td> ${cadaAluno} <td>
+    </tr>
+    ;
+    //console.log(`${contador+1} - ${cadaAluno}`);
+    contador++
+});
