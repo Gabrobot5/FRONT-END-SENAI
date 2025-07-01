5a2390238408ffaf1e5ddb489cd33a47f29fab62
Gabrobot5 <gabz343ind@gmail.com>
2025-07-01 11:41:54 -0300

    revisão

--- 04 - javascript/revisão 1/scripts/script.js
+++ 04 - javascript/revisão 1/scripts/script.js
@@ -1,3 +1,18 @@
 function ex1(){
-    alert("teste");
+    const enderecoInput = document.querySelector('#enderecoInput');
+    let link = "https://" + enderecoInput.value;
+
+    location = link
+
 }
+function ex2(){
+    let link = document.querySelector("#links").value;
+
+    location = link;
+}
+document.querySelector("#links").addEventListener('change',ex2)
+
+
+document.querySelector("#perc").addEventListener('focusout', () =>{
+    const valor =
+})
